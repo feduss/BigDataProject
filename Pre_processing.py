53d36fd36b4e1747cf354f46a23d8ba79441e91e
feduss <feduss96@gmail.com>
2020-01-29 13:06:55 +0100

    Implementazione preprocessing

---
+++ Pre_processing.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.preprocessing as pp
+
+#dataset = sns.load_dataset("credicard.csv")
+dataset = pd.read_csv('creditcard.csv') #Apro il Dataset come Panda DataFrame
+
+#Calcolo il numero di frodi e non frodi presenti
+no_frauds = len(dataset[dataset['Class'] == 0])
+frauds = len(dataset[dataset['Class'] == 1])
+
+#Calcolo gli indici dei valori indicati come frodi e non frodi
+non_fraud_indices = dataset[dataset.Class == 0].index
+fraud_indices = dataset[dataset.Class == 1].index
+
+#Prendo indici di non frodi randomicamente, ma ne scelgo lo stesso numero di quelli delle frodi, per bilanciamento
+random_indices = np.random.choice(non_fraud_indices, frauds, False)
+
+#Concateno gli indici delle frodi con i nuovi indici delle non frodi
+under_sample_indices = np.concatenate([fraud_indices, random_indices])
+
+#Ottengo il nuovo dataset undersampled
+under_sample = dataset.loc[under_sample_indices]
+
+norm_sample = under_sample
+
+preprocessing_status = True
+
+#Normalizzo i dati delle colonne delle transazioni (V1, V2, ...)
+for name_class in dataset.columns:
+    if str(name_class).startswith("V"):
+        print ("Colonna:" + name_class, end="\r")
+        x = under_sample[[name_class]].values.astype(float)
+        min_max_scaler = pp.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(x)
+        norm_sample[name_class] = x_scaled
+
+        max_value = norm_sample[name_class].max()
+        min_value = norm_sample[name_class].min()
+        if(max_value > 1.1 or min_value < -1.1):
+            print("La colonna " + name_class + " non è normalizzata")
+            print("Valore max: " + str(max_value) +", valore min: " + str(min_value))
+            preprocessing_status = False
+
+
+if(preprocessing_status):
+    print("Preprocessing eseguito correttamente")
